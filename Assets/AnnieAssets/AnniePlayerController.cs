37b8c276684ba815e961256a1f3810c676d7ee28
Annie Xu <axu.1492@gmail.com>
2019-04-16 15:35:09 -0700

    scene setup

---
+++ Assets/AnnieAssets/AnniePlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnniePlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+		rb.freezeRotation = true;
+    }
+
+    void FixedUpdate ()
+    {
+	if (Input.GetKey(KeyCode.RightArrow)) {
+		rb.velocity = transform.right * speed;
+	}
+	
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+		rb.velocity = -transform.right * speed;
+	}
+	
+	if (Input.GetKey(KeyCode.UpArrow)) {
+		rb.velocity = new Vector3(0, 0, 1) * speed;
+	}
+	
+	if (Input.GetKey(KeyCode.DownArrow)) {
+		rb.velocity = new Vector3(0, 0, -1) * speed;
+	}
+		
+        /*float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);*/
+    }
+}
---
+++ Assets/AnnieAssets/EnemySight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+	
+	public float FieldOfViewAngle = 110f;
+	public bool PlayerInSight;
+	public float AdjustRaycastHeight = 1f;
+	//public Vector3 PlayerLastSighting;
+	
+	private GameObject Player;
+	private SphereCollider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectsWithTag("Player")[0];
+		col = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerStay(Collider other) {
+		Debug.Log("collision");
+		if(other.gameObject == Player) {
+			PlayerInSight = false;
+			Debug.Log("Player");
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+			
+			if (angle < FieldOfViewAngle * 0.5f) {
+				RaycastHit hit;
+				if (Physics.Raycast(transform.position + (transform.up*AdjustRaycastHeight), direction.normalized, out hit, col.radius)) {
+					if (hit.collider.gameObject == Player) {
+						PlayerInSight = true;
+						Debug.Log("Player in view");
+					}
+				}
+			}
+		}
+	}
+}
