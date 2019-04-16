b59611124e769cc276d4ab08658c4d2c306b0346
Jesse <jesseallas@gmail.com>
2019-04-16 16:09:46 -0700

    made main menu

---
+++ Assets/StartAndExit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartAndExit : MonoBehaviour
+{
+    public bool IsExitBtn;
+    public int SceneToLoad;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+        if (IsExitBtn)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
