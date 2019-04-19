56460b07546e9a4ad139df16962980a27e2f47cd
Santiago Morales <31462534+Aleroms@users.noreply.github.com>
2019-04-18 17:24:14 -0700

    commiting on to santi branch...hopefully...

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -20,41 +20,156 @@ public class Enemy_Movement : MonoBehaviour
 	[SerializeField] private bool movement; //boolean flag that determine if
 	[SerializeField] private bool rotation; // a function is called
 	[SerializeField] private bool clockwise;
+	private bool pause_bool;
 
-	[SerializeField] private int pause;
+	[SerializeField] private float pause;
 
 	[SerializeField] private float speed;
 	[SerializeField] private float angle;
 
+	//private float init_angle;
+	//private float final_angle;
+	private float delta_angle;
+	[SerializeField] float targetAngle;
+	[SerializeField] float timer;
+
+	void Awake()
+	{
+		//initially angle is 0 and it will count upwards
+		delta_angle = angle;
+		timer = pause;
+		pause_bool = false;
+		//time = Time.time;
+	}
+
 	void Update()
 	{
+		//testing the code below
+		//Debug.Log(angle -= Time.deltaTime * speed);
+		
+
+
 		if(movement)
 		{
 			//code for movement goes here
 		}
 		else if(rotation)
 		{
-			Rotation(pause, speed, angle);
+			targetAngle = transform.eulerAngles.y + angle;
+			if (targetAngle > 180)
+			{
+				targetAngle -= 360;
+			}
+			//if > 180, subtract by 360;
+			StartCoroutine("RotationCoroutine");
+			rotation = false;
+		}
+		else
+		{
+			if(transform.eulerAngles.y == targetAngle && !pause_bool)
+			{
+				pause_bool = true;
+				timer = pause;
+			}
+			if (pause_bool)
+			{
+				timer -= Time.deltaTime;
+				if (timer < 0)
+				{
+					rotation = true;
+					pause_bool = false;
+				}
+
+			}
+		}
+	}
+	void Rotation()
+	{
+		if(!clockwise)
+		{
+			transform.Rotate(0f, speed * Time.deltaTime, 0f);
+		}
+
+	}
+
+	IEnumerator RotationCoroutine()
+	{   //float tar = 0 + 90 first loop
+		Vector3 _startAngle = transform.eulerAngles;
+		Vector3 _targetAngle = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + angle, transform.eulerAngles.z); //gets the current angle in y-axis + angle desired
+		Vector3 currentAngle = _startAngle;
+		while (currentAngle != _targetAngle)
+		{
+			this.transform.Rotate(0, speed * Time.deltaTime, 0);
+			if (this.transform.eulerAngles.y >= _targetAngle.y)
+			{
+				this.transform.eulerAngles = _targetAngle;
+				currentAngle = _targetAngle;
+			}
+
+			yield return null;
+			//yield return new WaitForSeconds(pause);
 		}
 	}
 
-	void Rotation(int pause = 0, float speed = 4.20f, float _angle = 0f)
+	void Rotation( float pause = 0, float speed = 4.20f, float _angle = 0f)
 	{
 		if (!clockwise)
 		{
-			transform.Rotate(0, Time.deltaTime * speed, 0);//x , y , z
-			if(transform.position == Vector3.up * )
-			StartCoroutine(PauseRotation());
-		}
+			//if(transform.rotation.eulerAngles.y < _angle)
 			
-		else		
-			transform.Rotate(0, Time.deltaTime * speed * -1, 0);
+
+			if (transform.rotation.eulerAngles.y % _angle == 0f && !pause_bool)
+			{
+				pause_bool = true;
+				timer = pause;
+			}
+			if (pause_bool)
+			{
+				timer -= Time.deltaTime;
+				if (timer < 0)
+				{
+					print(timer);
+					pause_bool = false;
+					transform.Rotate(0f, speed * Time.deltaTime, 0f);
+				}
+
+			}
+			else
+				transform.Rotate(0f, speed * Time.deltaTime, 0f);
+			
+
+
+			//else
+
+
+			//StartCoroutine(PauseRotation());
+
+		}
+
+		//else		
+			//transform.Rotate(0, Time.deltaTime * speed * -1, 0);
 	}
 	IEnumerator PauseRotation()
 	{
+		print("inside coroutine");
 		yield return new WaitForSeconds(pause);
 	}
 }
 //Garbage code that I might want to use later
 //transform.Rotate(Vector3.up * Time.deltaTime * speed, _angle, Space.World);
 //transform.Rotate(Vector3.up* Time.deltaTime* speed * -1, _angle, Space.World);
+//transform.Rotate(0, Time.deltaTime * speed, 0);
+
+
+
+//transform.Rotate(0, Time.deltaTime * speed, 0);//x , y , z
+//if(transform.position == Vector3.up * )
+
+
+//Quaternion desiredRotation = Quaternion.Euler(0, angle, 0);
+//transform.Rotate(Vector3.up, _angle);
+			//transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, speed * Time.deltaTime);
+			//if(delta_angle >= 0)
+			//{
+			//	delta_angle -= Time.deltaTime;
+			//}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+	[SerializeField] private float angle;
+	//private float timer = 5f;
+	//private float reset;
+	//private bool pause = false; 
+	
+
+
+    void Awake()
+	{
+		//reset = timer;
+		
+		
+	}
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 initialState = this.transform.eulerAngles;
+		Vector3 targetState = new Vector3(initialState.x, initialState.y + angle, initialState.z);
+		//transform.rotation; 
+		Vector3 currentState = Vector3.Lerp(initialState, targetState, 0.05f);	
+		
+    }
+	void Rotate()
+	{
+		//pause = true;
+		//timer = reset;
+		transform.Rotate(Vector3.up);
+	}
+}
+
+/*
+ 
+	 print(timer);
+		timer -= Time.deltaTime;
+
+		if (timer > 0 && !pause)
+			Rotate();
+		else
+		{
+			pause = true;
+			timer = reset;
+		}
+	 
+	 */
