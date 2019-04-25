a8241531ef66d50c9f0b44a94f1d5dc79d00f8f0
Annie Xu <axu.1492@gmail.com>
2019-04-25 15:01:50 -0700

    scalable enemy sight

--- Assets/AnnieAssets/FieldOfVisionSetup.cs
+++ Assets/AnnieAssets/FieldOfVisionSetup.cs
@@ -9,6 +9,7 @@ public class FieldOfVisionSetup : MonoBehaviour {
 	public float fieldDepth = 10f; //radius of the collider
 	public int raysInCast = 100; //appearance of fieldView
 	public float enemyEyeHeight = 1f;
+	public GameObject enemyFront;
 
 	private EnemySight enemySight;
 	private FieldOfVision fieldOfVision;
@@ -22,8 +23,9 @@ public class FieldOfVisionSetup : MonoBehaviour {
 		fieldOfVision.setAngle(fieldViewAngle*Mathf.Deg2Rad/2); //in radians
 		
 		//set radius - need to adjust for weird scaling
+		Debug.Log(enemyFront.transform.localScale.x);
 		enemySight.setColliderRadius(fieldDepth);
-		fieldOfVision.setSightRange(fieldDepth/5);
+		fieldOfVision.setSightRange(fieldDepth/(1/enemyFront.transform.localScale.x));
 		
 		
 		//set the raycast height
