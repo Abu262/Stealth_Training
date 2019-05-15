8ed153642c6ae44ae50e260c4b20d0dc7f96a6fd
Jesse <jesseallas@gmail.com>
2019-05-14 20:25:36 -0700

    visionthingy

--- Assets/FixView.cs
+++ Assets/FixView.cs
@@ -26,18 +26,31 @@ public class FixView : MonoBehaviour
       _LastTransforms.Clear();
     }
 
+        RaycastHit[] hits = Physics.BoxCastAll(
+            transform.position, 
+            new Vector3(1f, 1f, 1f), 
+            WatchTarget.transform.position - transform.position,
+            new Quaternion(0,0,0,1),
+            Vector3.Distance(WatchTarget.transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z +3f)), 
+            OccluderMask);
+//      transform.position,
+  //    WatchTarget.transform.position - transform.position,
+    //  Vector3.Distance(WatchTarget.transform.position, transform.position),
+      //OccluderMask
+
     //Cast a ray from this object's transform the the watch target's transform.
-    RaycastHit[] hits = Physics.RaycastAll(
-      transform.position,
-      WatchTarget.transform.position - transform.position,
-      Vector3.Distance(WatchTarget.transform.position, transform.position),
-      OccluderMask
-    );
+ //   RaycastHit[] hits = Physics.RaycastAll(
+   //   transform.position,
+     // WatchTarget.transform.position - transform.position,
+     // Vector3.Distance(WatchTarget.transform.position, transform.position),
+      //OccluderMask
+    
 
     //Loop through all overlapping objects and disable their mesh renderer
     if(hits.Length > 0){
       foreach(RaycastHit hit in hits){
-        if(hit.collider.gameObject.transform != WatchTarget && hit.collider.transform.root != WatchTarget){
+        if(hit.collider.gameObject.transform != WatchTarget && hit.collider.transform.root != WatchTarget && Vector3.Distance(transform.position, hit.collider.transform.position) < Vector3.Distance(transform.position, WatchTarget.transform.position))
+                {
                     if (hit.collider.gameObject.GetComponent<Renderer>() != null)
                     {
                         _LastTransforms.Add(hit.collider.gameObject.transform, hit.collider.gameObject.GetComponent<Renderer>().material);
