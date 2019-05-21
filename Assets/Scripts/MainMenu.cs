3200bc9d0889e7b16c1e2eb0a365f1c9690f7a4e
Santiago Morales <31462534+Aleroms@users.noreply.github.com>
2019-05-21 16:27:05 -0700

    UI stuff almost completed?

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void Mission()
+	{
+		SceneManager.LoadScene("MissionMenu");
+	}
+	public void Controls()
+	{
+		SceneManager.LoadScene("ControlMenu");
+	}
+	public void Credits()
+	{
+		SceneManager.LoadScene("CreditsMenu");
+	}
+	public void Quit()
+	{
+		print("Application has quit");
+		Application.Quit();
+	}
+}
